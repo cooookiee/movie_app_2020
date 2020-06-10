d809ae2aee859b7739dbb96e159cb8fe51744ccc
cooookiee <cookie292@gmail.com>
2020-06-10 15:31:44 +0900

    Updates

---
+++ precache-manifest.f48dda74ef8d08c285df9a5e2e8908cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0422a7496a6a0f42d67384a8eb002829",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "12c7db083c993e7bbaa9",
+    "url": "/movie_app_2020/static/css/main.c5bfdd40.chunk.css"
+  },
+  {
+    "revision": "c50adcdf695eba86cb7d",
+    "url": "/movie_app_2020/static/js/2.af139e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.af139e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c7db083c993e7bbaa9",
+    "url": "/movie_app_2020/static/js/main.8fdc8129.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
